9cbcf73e77ce02a6901d5a22851ce60f3bb77cf6
JasonQu <qjpcpu@gmail.com>
2019-03-13 20:27:23 +0800

    reopen file when deleted or moved

--- filelog.go
+++ filelog.go
@@ -145,10 +145,23 @@ func (w *FileLogWriter) watchFile(filename string) {
 		if err = wa.AddWatch(filename, syscall.IN_DELETE|syscall.IN_DELETE_SELF|syscall.IN_MOVE|syscall.IN_MOVE_SELF|syscall.IN_IGNORED); err != nil {
 			break
 		}
+		wa.AddWatch(filepath.Dir(filename), syscall.IN_DELETE)
 		go func(iw *inotify.Watcher) {
-			select {
-			case <-iw.Event:
-			case <-iw.Error:
+		LOOP:
+			for {
+				select {
+				case ev := <-iw.Event:
+					if ev.Mask == syscall.IN_DELETE {
+						abs, _ := filepath.Abs(ev.Name)
+						if abs == filename {
+							break LOOP
+						}
+					} else {
+						break LOOP
+					}
+				case <-iw.Error:
+					break LOOP
+				}
 			}
 			atomic.CompareAndSwapInt32(&w.reOpen, 0, 1)
 			iw.Close()
